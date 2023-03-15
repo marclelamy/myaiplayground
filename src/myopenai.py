592f6c74e0947eeda716a42e4b73fac11b2a5942
Marc Lamy <marclelamym@gmail.com>
2023-03-15 18:05:29 -0400

    added streaming

--- src/myopenai.py
+++ src/myopenai.py
@@ -3,17 +3,26 @@ import openai
 import pandas as pd
 import numpy as np
 import sqlite3 
-import json
-import os 
-import requests
 from datetime import datetime
 import tiktoken
 import streamlit as st
 import base64
+import os 
+import json
+
+
+
+
+
+
+
+
+
 
 def check_openai_api_key(api_key):
-    if api_key == '':
-        return ":red[No Openai API key provided yet]", False
+    '''
+    Checks if the provided OpenAI API key is valid by attempting to list the available models.
+    '''    
     openai.api_key = api_key
     try: 
         openai.Model.list()
@@ -21,245 +30,65 @@ def check_openai_api_key(api_key):
     except Exception as e:
         return f"Openai API key is :red[invalid or not working]: {e}", False
 
-
-def input_api_key():
-    # st.write(st.session_state)
-    # if 'openai_api_key_input' in st.session_state:
-    #     del st.session_state['openai_api_key_input']
-    # st.write(st.session_state)
-    st.session_state['OPENAI_API_KEY'] = st.sidebar.text_input('Enter your OpenAI API Key to use the models. Find it [here](https://platform.openai.com/account/api-keys).')
-    # st.write(st.session_state)
-
 def sidebar_api_input(): 
-    # st.sidebar.write(st.session_state['OPENAI_API_KEY'])
-    # st.sidebar.write('OPENAI_API_KEY' in st.session_state)
-    # st.sidebar.write(st.session_state['OPENAI_API_KEY'] not in (None, ''))
-    if 'OPENAI_API_KEY' not in st.session_state:
-        st.session_state['OPENAI_API_KEY'] = ''
-    if st.session_state['OPENAI_API_KEY'] == '':
-        input_api_key()
-    
-
-    message, valid = check_openai_api_key(st.session_state['OPENAI_API_KEY'])
-    st.sidebar.markdown(message)
-    if valid == False and st.session_state['OPENAI_API_KEY'] != '':
-        input_api_key()
+    '''Displays a text input widget in the Streamlit sidebar for users to enter their OpenAI API key.
+    If the key is already set in the Streamlit secrets, it will be used to check if the key is valid 
+    using the `check_openai_api_key()` function.
+    '''
+    if 'OPENAI_API_KEY' not in st.secrets:
+        st.secrets['OPENAI_API_KEY'] = ''
+    if st.secrets['OPENAI_API_KEY'] == '':
+        st.secrets['OPENAI_API_KEY'] = st.sidebar.text_input('Enter your OpenAI API Key to use the models. Find it [here](https://platform.openai.com/account/api-keys).')
+    else: 
+        message, valid = check_openai_api_key(st.secrets['OPENAI_API_KEY'])
+        st.sidebar.markdown(message)
 
 
+    if valid: 
+        st.session_state['api_valid'] = True
+        openai.api_key = st.secrets['OPENAI_API_KEY']
 
 
-# lst = [{"role": "user", "content": "So I've been worried an article about the app that I just made. So I'm currently making a streamlined application with a bunch of AI APIs. So I'm using chat GPT API, Dali API, Whisper API. So like those are different models for like text, image, and speech to text. And they also use stable diffusion, which is a model that takes text and image. And so I'm making, so like all those different tools are like on different websites, different platforms and stuff. And so what I'm doing is pretty much grouping everything into one single web app where people can just enter the API key and just start playing with all the APIs without having to do much else. They can also like store all their data locally. Everything is like when they start a session, there's a database that is created. And all the image generated also saved just like build your house. And so I want you to help me write an article about it. Propose me another line, maybe ask me more questions for clarification and I will answer" },
-#   {"role": "assistant", "content": "Sure, I'd be happy to help you write an article about your new app. Can you tell me more about how users can benefit from this app? For example, in what ways can the AI APIs improve their work or daily tasks?" },
-# {"role": "user", "content": "Well, for example, one thing could be, well, chat.jpg, it's the use, so that's great. Delijous screen image, that's fun. And then Whisper, you can translate any, for example, like audio, so like the audio of a video, for example. And then you can just extract Jojo and then just convert it into text, which is great, for example, for clouds or anything." },
-# {"role": "assistant", "content": "That's really interesting! So, it sounds like your app can help users with various tasks, such as transcribing audio, recognizing images, and even analyzing text? How does your app streamline these processes compared to users manually accessing each API separately? Can you speak more to the ease of use that your app offers?" },
-# {"role": "user", "content": "an article written by chat gpt after interviewing me by talking to it (with whisper) about the webapp that I made and from which I got interviewed." },
-# {"role": "assistant", "content": "That's a unique idea! By interviewing you through the app, we can showcase the app's capability in real-time. We can then describe the conversation between you and the AI, highlighting the features and benefits of the app that you discuss. For this, I'm going to ask you multiple questions about the app and then write a full outline, is that okay?" }]
-lst = []
+
+
+
+
+
 
 class Gpt(): 
     def __init__(self, api_key=None):
         self.api_key = api_key
-        self.conversation_id = np.random.randint(10**12)
-        self.messages = lst
+        self.messages = []
         self.past_question = ''
-        self.hide_message_map = []
-
-
-    def add_user_input(self, message): 
-        self.log_question(message)
-        self.hide_message_map.append(1)
-        message = {'role': 'user', 'content': message}
-        if message != self.messages:
-            self.messages.append(message)
-
-
-
-    def ask(self, message):
-        '''Ask a question to the model'''
-        self.past_question = message
-        response = self.request()
-        chat_answer = []
-        # print(response.keys())
-        # for key in response.keys():
-        #     print(key, response[key], '\n')
-        # print(f'\n\n\n{response.keys() = }\n\n\n')
-        for chat_message in response['choices']:
-            # print(chat_message)
-            message = chat_message['message']
-            message['index'] = chat_message['index']
-            self.messages.append(message)
-            chat_answer.append(message['content'])
-
-        self.log_answer(response)
-        return chat_answer
-
+        self.conversation_id = np.random.randint(10**10)
     
+    def add_message(self, role, message_content):
+        message = {'role': role, 'content': message_content}
+        self.messages.append(message)   
 
-    def request(self): 
-        '''Makes a request to Open AI'''
-        # print(self.message)
-        # print('\n\n'*3)
-        # for message in self.messages:
-        #     print(message)
+    # def log_message(self, role, message_content):
+    #     data = {'role': [role]
+    #             'content': [message_content]
+    #             'datetime': [datetime.now()]
+    #             }
+        
+    #     df = pd.DataFrame(data)
 
-        messages = [{i:j for i, j in message.items() if i in ('role', 'content')} for index, message in enumerate(self.messages)]
 
+    def ask(self, params): 
+        self.request_id = np.random.randint(10**10)
         completion = openai.ChatCompletion.create(
-            model = "gpt-3.5-turbo", 
-            messages = messages,
-            temperature = self.temperature,
-            n = 1,
-            # n = self.n_answers,
+            model = "gpt-3.5-turbo-0301", 
+            messages = self.messages,
+            **params
             )
-        # print(completion.keys())
-        return completion.to_dict()
-
-
-
-    
-    def new_conversation(self): 
-        '''Reset some parameters like conversation id and stuff'''
-
-
-
-
-
-    def log_question(self, message): 
-        '''Add a message to the conversation'''
-        self.con = sqlite3.connect('data/database.sqlite')
-        pd.DataFrame({'content': [message], 'conversation_id': [self.conversation_id], 'datetime': [datetime.now()]}).to_sql('questions', self.con, if_exists='append', index=False)
-
-
-
-    def log_answer(self, data): 
-        '''Add a message to the conversation'''
-        # st.write(data)
-        # print(data)
-        self.con = sqlite3.connect('data/database.sqlite')
-        data.update(data['usage'])
-        data.pop('usage')
-        df_answers = pd.DataFrame(data['choices'])
-        data.pop('choices')
-        df_information = pd.DataFrame([data])
-        df_information['conversation_id'] = self.conversation_id
-
-        df_answers = pd.concat([df_answers, pd.json_normalize(df_answers['message'])], axis=1)
-        df_answers = df_answers.drop('message', axis=1)
-        df_answers['prompt_id'] = data['id']
-        df_answers['conversation_id'] = self.conversation_id
-
-        df_information.to_sql('information', self.con, if_exists='append', index=False)
-        df_answers.to_sql('answers', self.con, if_exists='append', index=False)
-        return df_answers
-
-
-    def num_tokens_from_string(self, string: str, model_name: str) -> int:
-        """Returns the number of tokens in a text string."""
-        encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
-        num_tokens = len(encoding.encode(string))
-        return num_tokens
+        
+        return completion
     
 
-
-class Dalle(): 
-    def __init__(self, api_key=None):
-        # self.set_api_key()
-        self.api_key = api_key
-        self.conversation_id = np.random.randint(10**12)
-        self.con = sqlite3.connect('data/database.sqlite')
-        self.messages = []
-        self.past_question = ''
-
-
-
-    def imagine (self, 
-                 prompt, 
-                 n_image,
-                 image_size
-                 ):
+    def save_response(self, response):
+        if not os.path.exists('data/responses/'):
+            os.mkdir('data/responses/')
         
-        self.requestID = np.random.randint(10**12)
-        params = {
-            'prompt': prompt,
-            'n': n_image,
-            'size': image_size,
-            'response_format': 'b64_json'
-            }
-        
-        self.log_params(params)
-        response = self.request(params)
-        self.log_response(response)
-        return response
-    
-
-    def request(self, params): 
-        return openai.Image.create(
-                        params
-                        )
-
-
-
-    def log_params(self, params):
-        self.con = sqlite3.connect('data/database.sqlite')
-        df = pd.DataFrame([params])
-        df['conversation_id'] = self.conversation_id
-        df['request_id'] = self.requestID
-        df.to_sql('dalle_params', self.con, if_exists='append', index=False)
-
-
-    def log_response(self, response):
-        self.con = sqlite3.connect('data/database.sqlite')
-        
-        data = {'created': [], 'image_data': [], 'index': []}
-        for image_index, image_data in enumerate(response['data']):
-            data['created'].append(response['created'])
-            data['image_data'].append(image_data['b64_json'])
-            data['index'].append(image_index + 1)
-            data['conversation_id'] = self.conversation_id
-            data['request_id'] = self.requestID
-
-        pd.DataFrame(data).to_sql('dalle_response', self.con, if_exists='append', index=False)
-
-
-
-    def responses_to_images(self, response): 
-        images = []
-        for index, image_data in enumerate(response['data']):
-            image = base64.b64decode(image_data['b64_json'])
-            with open(f'image_{index + 1}.png', 'wb') as f:
-                f.write(image)
-
-
-        return images
-
-
-
-    # def download_image(self, image_url): 
-
-    #     return image
-
-
-
-
-
-
-
-
-
-
-class Whisper(): 
-    def __init__(self, api_key=None):
-        # self.set_api_key()
-        self.api_key = api_key
-        self.conversation_id = np.random.randint(10**12)
-        self.con = sqlite3.connect('data/database.sqlite')
-        self.messages = []
-        self.past_question = ''
-
-
-
-    def transcribe(self, message): 
-        transcription = openai.Audio.transcribe(
-            model = 'whisper-1',
-            file="test_audio.mp3"
-            )
+        with open(f'data/responses/{self.conversation_id}_{self.request_id}.json', 'w') as f:
+            response = json.dumps(response.to_dict())
--- src/pages/1_ChatGPT.py
+++ src/pages/1_ChatGPT.py
@@ -3,8 +3,6 @@ import pandas as pd
 from myopenai import Gpt, sidebar_api_input
 from pydub import AudioSegment
 import openai
-
-
 from audiorecorder import audiorecorder
 from pydub import AudioSegment
 import io
@@ -17,6 +15,15 @@ st.set_page_config(layout='wide',
                    page_title='Chat',
                    page_icon='🤖')
 
+
+# Check if API key is valid
+sidebar_api_input()
+
+if st.session_state.api_valid and 'gpt' not in st.session_state:
+    st.session_state.gpt = Gpt()
+    # openai.api_key = st.secrets['OPENAI_API_KEY']
+
+
  
 
 
@@ -25,181 +32,110 @@ st.set_page_config(layout='wide',
 ### MAIN AREA
 ##############################################################################################################
 st.title('Chat GPT')
-# st.markdown(check_openai_api_key(st.secrets['OPENAI_API_KEY'])[0])
+st.write('A chatbot powered by OpenAI GPT-3.5 Turbo.')
 
 
-col1, col2, col3 = st.columns([4, 4, 2])
-col1.write('A chatbot powered by OpenAI GPT-3.5 Turbo.')
 
 
-### TABS 
+
+
+
+# GAME AREA BELOW
 ##############################################################################################################
-# tab1, tab2 = st.tabs([, "Play"])
+st.markdown("<hr style='height:1px;border:none;color:#FFA500;background-color:#FFA500;' /> ", unsafe_allow_html=True)
+general_col1, general_col2 = st.columns([16, 3])
 
 
-# with tab2: 
-    # st.write('Learn about the model')
-    # st.markdown('''
-
-    # What to add to learning page: 
-    # * What is GPT 
-    # * What is NLP 
-    # * How to build it from scratch? Karparthy video 
-
-    # ''')
-    # st.title("The following list won’t indent no matter what I try:")
-    # st.header('The following list won’t indent no matter what I try:')
-    # st.subheader('The following list won’t indent no matter what I try:')
-    # st.write('The following list won’t indent no matter what I try:')
-    # st.markdown('The following list won’t indent no matter what I try:')
-    # st.code('print("The following list won’t indent no matter what I try:")', language='python')
-
-
-##############################################################################################################
-# THIS IS WHERE PLAYING STARTS
-##############################################################################################################
-# with tab2: 
-if st.button('Clear'):
-    for key in st.session_state.keys():
-        if key not in ('OPENAI_API_KEY'):
-            del st.session_state[key]
-        
-        if key == 'gpt': 
-            st.session_state.gpt = Gpt(st.secrets['OPENAI_API_KEY'])
-
-# st.write(st.session_state)
-
-
-### SIDEBAR
-##############################################################################################################
-sidebar_api_input()
-
-if 'gpt' not in st.session_state:
-    st.session_state.gpt = Gpt(st.secrets['OPENAI_API_KEY'])
-if 'user_input' not in st.session_state:
-    st.session_state.user_input = ''
-if 'show_message_index' not in st.session_state:
-    st.session_state.gpt.show_message_index = []
-
-st.session_state.gpt.messages = []
-for _ in range(3):
+# Model Parameters 
+with general_col2:
+    st.write(' ') #This an dnext are to allign the button with the text area
     st.write(' ')
+    with st.expander('Parameters'):
+        # input1, input2, input3 = st.columns([1, 1, 1])
+        n_answers = st.slider('Answers to generate', 
+                              min_value=1, max_value=10, value=1, step=1,
+                              help='How many chat completion choices to generate for each input message.\nWhen selecting multiple answers, all answers will be displayed at the same time which may take more time to load.\nIt\'s recommended to select the number of answers you want to see before asking the chatbot a question.'
+                                )
+        temperature = st.slider('Temperature', 
+                                    min_value=0.0, max_value=2.0, value=1.0, step=.01,
+                                    help='Higher values like 0.8 will make the output more random\nwhile lower values like 0.2 will make it more focused and deterministic.\nWe generally recommend altering this or `top_p` but not both.'
+                                    )
+        
+        params = {
+            'temperature': temperature,
+            # 'top_p': 1,
+            'n': 1,
+            'stream': True,
+            # 'max_tokens': 100,
+            # 'presence_penalty': 0,
+            # 'frequency_penalty': 0,
+        }
 
 
 
-
-
-
-# Prompts Parameters
-############################################################################################################## 
-input1, input2, input3 = st.expander('Parameters').columns([1, 1, 1])
-df_prompts = pd.read_csv('data/prompts.csv')
-df_prompts.loc[len(df_prompts)] = ['None', '']
-df_prompts.loc[len(df_prompts)] = ['', '']
-
-### Prompt selecton
-test_area_prompt = input1.selectbox(
-        "Chose a prompt for the chatbot to 'act as'",
-        [''] + df_prompts['act_as'].sort_values().tolist())
-test_area_prompt = '' if test_area_prompt == 'None' else test_area_prompt
-prompt = df_prompts[df_prompts['act_as'] == test_area_prompt].iloc[0, 1] + ''
-# input1.write(prompt + ' The more detailed you can be, the better.')
-
-
-
-
-
-
-### Model Parameters
-##############################################################################################################
-n_answers = input2.slider('How many answers do you want?', 
-                        min_value=1, 
-                        max_value=10, 
-                        value=1, 
-                        step=1,
-                        help='The number of answers you want to get from the chatbot.\nWhen selecting multiple answers, all answers will be displayed at the same time which may take more time to load.\nIt\'s recommended to select the number of answers you want to see before asking the chatbot a question.'
-                        )
-st.session_state.gpt.n_answers = n_answers
-temperature = input3.slider('what temperature do you want?', 
-                            min_value=0.0, 
-                            max_value=2.0, 
-                            value=1.0, 
-                            step=.01,
-                            help='The temperature of the chatbot. The higher the temperature, the more creative the answers will be.'
-                            )
-st.session_state.gpt.temperature = temperature
-
-
-
-
-
-
-# INPUT AREA
-##############################################################################################################
-col1, col2 = st.columns([16, 3])
-
-# COST
-prompt_cost = round(st.session_state.gpt.num_tokens_from_string(st.session_state.user_input, 'gpt-3.5-turbo') / 1000 * 0.002, 5)
-# col2.write(prompt_cost)
-col2.write(' ')
-with col2:
+# MIC
     audio = audiorecorder("Click to record", "Recording...")
     transcript = ''
     if len(audio) > 0:
-        byte_audio = audio.tobytes()
-        # Export the AudioSegment to a WAV file
-        AudioSegment.from_file(io.BytesIO(byte_audio)).export('output_microphone.mp3', format='mp3')
+        AudioSegment.from_file(io.BytesIO(audio.tobytes())).export('output_microphone.mp3', format='mp3')
         audio_file = open("output_microphone.mp3", "rb")
         transcript = openai.Audio.transcribe("whisper-1", audio_file)['text']
 
-
 # TEXT INPUT
-value = transcript if transcript != '' else prompt
-label = "Talk to chat - For technical reasons, you can't send twice the same message but adding a space somewhere fixes it." 
-st.session_state.user_input = col1.text_area(label, key='text_area', value=value)
+with general_col1:
+    label = "Talk to chat - For technical reasons, you can't send twice the same message but adding a space somewhere fixes it." 
+    prompt = '' # modify later
+    value = transcript if transcript != '' else prompt
+    user_input = st.text_area(label, key='text_area', value=value)
 
 
 
 
 
+chat_conversation = st.empty()
+if user_input not in ('', st.session_state.gpt.past_question): 
+
+    # st.write(user_input)
+    st.session_state.gpt.add_message('user', user_input)
+    st.session_state.gpt.past_message = user_input
+    with st.spinner('Asking Chat...'):
+        response = st.session_state.gpt.ask(params)
+    
+    # if params['stream']: 
+    with general_col1:
+        collected_messages = []
+        chat_message = st.empty()            
+        for index, chunk in enumerate(response):
+            chat_message.empty()
+            chunk_message = chunk['choices'][0]['delta']  
+            collected_messages.append(chunk_message) 
+
+            reply = ''.join([m.get('content', '') for m in collected_messages])
+            # chat_message.caption(f'Chat:')
+            chat_message.write(reply)
+            # chat_message.markdown("<hr style='height:1px;border:none;color:#FFFFFF;background-color:#FFFFFF;' /> ", unsafe_allow_html=True)
+
+        chat_message.empty()
+        # data = {'role': ['user']
+        #         'content': [message_content]
+        #         'datetime': [datetime.now()]
+        #         }
+ 
+    # else: 
+    #     #add response to all messages and display all 
+    #     reply 
+    #     st.session_state.gpt.save_response(response)
 
 
 
-# Create conversation container
-if 'conversation_chat_container' not in st.session_state:
-    st.session_state.conversation_chat_container = st.empty()
+    st.session_state.gpt.add_message('assistant', reply)
 
 
-# Make the request and display chat
-##############################################################################################################
-if st.session_state.user_input not in ('', test_area_prompt, prompt, st.session_state.gpt.past_question):
-    st.session_state.gpt.add_user_input(st.session_state.user_input)
-
-    # Loop to get one answer per iteration
-    for nn in range(n_answers):
-        with st.session_state.conversation_chat_container.container():
-            # Make the request
-            print('Making the request...')
-            with st.spinner('Asking Chat...'):
-                st.session_state.gpt.show_message_index += [1]
-                st.session_state.gpt.ask(st.session_state.user_input)
-
-            col_user, col_messages, col_checkbox = st.columns([5, 2, 2])
-            # Display conversation
-            person = {'user': 'Me', 'assistant': 'Chat'}
-            for message_index, message in enumerate(st.session_state.gpt.messages[::-1]):
-                message_content = message['content']
-                
-                with col_user:
-                    who = person[message['role']]
-                    st.caption(f'{who}:')
-                    st.write(message_content)
-                    st.markdown("<hr style='height:1px;border:none;color:#FFFFFF;background-color:#FFFFFF;' /> ", unsafe_allow_html=True)
-
-
-            # Display answer
-            # print(f'{st.session_state.gpt.show_message_index = } - last line')
-
-# Write a cringe LinkedIn post about how I'm so excited about using chat GPT and whisper through their API in a streamlit web app.
-
-
+    for message_index, message in enumerate(st.session_state.gpt.messages[::-1]):
+        message_content = message['content']
+        
+        with general_col1:
+            who = {'user': 'Me', 'assistant': 'Chat'}[message['role']]
+            st.caption(f'{who}:')
+            st.write(message_content)
+            st.markdown("<hr style='height:1px;border:none;color:#FFFFFF;background-color:#FFFFFF;' /> ", unsafe_allow_html=True)
